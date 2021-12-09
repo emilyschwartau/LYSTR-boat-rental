b047e21571de4ef6a21a8a29856c2d887bb20430
Austin Kammerer <austinkammerer@Austins-MacBook-Pro.local>
2021-12-09 17:31:21 -0600

    initial database table creation commands

--- database.sql
+++ database.sql
@@ -26,6 +26,13 @@ CREATE TABLE "vehicle" (
     "daily_rate" INTEGER NOT NULL,
     "cabins" INTEGER NOT NULL,
     "heads" INTEGER NOT NULL,
+    "instructions" VARCHAR(1000),
+);
+
+-- vehicle type table - mainly for generating select options
+CREATE TABLE "vehicle_type" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
 );
 
 -- listed vehicles' addresses go here:
@@ -39,8 +46,45 @@ CREATE TABLE "vehicle_address" (
     "address_type_id" INTEGER NOT NULL REFERENCES "address_type" ON DELETE CASCADE,
 );
 
--- address types (dock, driveway, etc)
-CREATE TABLE "address_type" (
+-- photos for each vehicle
+CREATE TABLE "photos" (
     "id" SERIAL PRIMARY KEY,
+    "vehicle_id" INTEGER NOT NULL REFERENCES "vehicle" ON DELETE CASCADE,
+    "image_path" VARCHAR(510) NOT NULL,
+);
 
+-- vehicle features (boolean values, used to generate inputs)
+CREATE TABLE "features" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+);
+
+-- junction table
+CREATE TABLE "vehicle_features" (
+    "id" SERIAL PRIMARY KEY,
+    "vehicle_id" INTEGER NOT NULL REFERENCES "vehicle" ON DELETE CASCADE,
+    "feature_id" INTEGER NOT NULL REFERENCES "features" ON DELETE CASCADE,
+);
+
+-- table of dates that vehicles are available to rent
+CREATE TABLE "availability" (
+    "id" SERIAL PRIMARY KEY,
+    "vehicle_id" INTEGER NOT NULL REFERENCES "vehicle" ON DELETE CASCADE,
+    "date" DATETIME NOT NULL,
+    "is_rented" BOOLEAN DEFAULT FALSE,
+);
+
+-- holds rental apointments
+CREATE TABLE "rental" (
+    "id" SERIAL PRIMARY KEY,
+    "rented_by" INTEGER NOT NULL REFERENCES "user" ON DELETE CASCADE,
+    "date_id" DATETIME NOT NULL REFERENCES "availability" ON DELETE CASCADE,
+);
+
+-- STRETCH table for messages between users
+CREATE TABLE "messages" (
+    "id" SERIAL PRIMARY KEY,
+    "sent_by" INTEGER NOT NULL REFERENCES "user" ON DELETE CASCADE,
+    "received_by" INTEGER NOT NULL REFERENCES "user" ON DELETE CASCADE,
+    "msg" VARCHAR(1000) NOT NULL,
 )
