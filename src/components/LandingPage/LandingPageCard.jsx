bde43404995ca5b80b0e69f94c6509683b65fb0a
Chaoching Vang <chaoching.vang@gmail.com>
2021-12-13 12:49:01 -0600

    added date picker

--- src/components/LandingPage/LandingPageCard.jsx
+++ src/components/LandingPage/LandingPageCard.jsx
@@ -1,7 +1,24 @@
+import React from 'react';
 import { Box, Card, Typography, Button, TextField, FormControl } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import { format } from 'date-fns';
 
 function LandingPageCard() {
+    const [startDate, setStartDate] = React.useState(new Date());
+
+    const handleChange = (newValue) => {
+        const formattedStartDate = format(newValue, 'MM/dd/yy');
+        console.log(`this is format`, formattedStartDate);
+        setStartDate(formattedStartDate)
+    };
+
+
+
+    console.log(startDate);
+
     return (<>
         <Box sx={{
             width: '50%'
@@ -12,7 +29,7 @@ function LandingPageCard() {
                 padding: '1em',
                 textAlign: 'center'
             }}>
-                <Typography variant='h4'>Find A Boat Near You!</Typography>
+                <Typography variant='h4'>Find A Boat To Rent Near You!</Typography>
                 <br />
                 <FormControl
                     fullWidth={true}
@@ -22,6 +39,19 @@ function LandingPageCard() {
                         helperText='Search Location by City, State'
                         label='Location'
                     />
+                    <br />
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            label='Date of Trip'
+                            helperText='Date of Trip'
+                            value={startDate}
+                            onChange={((newValue) => {
+                                handleChange(newValue);
+                            })}
+                            renderInput={(params) => <TextField {...params} />}
+                        />
+                    </LocalizationProvider>
+                    <br />
                     <Button
                         variant='outlined'
                         endIcon={<SearchIcon />}
