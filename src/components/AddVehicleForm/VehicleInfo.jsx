3cb2c69b57a81e7865b468607e3c222ce37114e2
AustinKammerer <austin4191@gmail.com>
2021-12-13 11:57:49 -0600

    VehicleInfo form component

--- src/components/AddVehicleForm/VehicleInfo.jsx
+++ src/components/AddVehicleForm/VehicleInfo.jsx
@@ -8,6 +8,7 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import Typography from "@mui/material/Typography";
 
 export default function VehicleInfo() {
   const dispatch = useDispatch();
@@ -31,6 +32,11 @@ export default function VehicleInfo() {
   console.log(vehicle);
   return (
     <Grid container maxWidth="md" mx="auto" direction="column">
+      <Grid item>
+        <Typography component="h2" variant="h5">
+          Vehicle Info
+        </Typography>
+      </Grid>
       <Grid item>
         <FormControl margin="normal" fullWidth>
           <TextField
@@ -78,6 +84,54 @@ export default function VehicleInfo() {
               name="model"
               variant="standard"
               label="Model"
+              required
+              onChange={handleChange}
+            />
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid item container spacing={2}>
+        <Grid item sm={6}>
+          <FormControl margin="normal" fullWidth>
+            <TextField
+              name="year"
+              variant="standard"
+              label="Year"
+              required
+              onChange={handleChange}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item sm={6}>
+          <FormControl margin="normal" fullWidth>
+            <TextField
+              name="length"
+              variant="standard"
+              label="Length (ft)"
+              required
+              onChange={handleChange}
+            />
+          </FormControl>
+        </Grid>
+      </Grid>
+      <Grid item container spacing={2}>
+        <Grid item sm={6}>
+          <FormControl margin="normal" fullWidth>
+            <TextField
+              name="capacity"
+              variant="standard"
+              label="Capacity"
+              required
+              onChange={handleChange}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item sm={6}>
+          <FormControl margin="normal" fullWidth>
+            <TextField
+              name="horsepower"
+              variant="standard"
+              label="Horsepower"
               required
               onChange={handleChange}
             />
