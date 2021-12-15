e793785ebfcda986f1c4819b148b72bfbca17826
Emily Schwartau <emilyschwartau+1@gmail.com>
2021-12-15 17:53:05 -0600

    can sign out, and links conditional, still need to figure out profile pic conditional rendering

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import "./Nav.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 //import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -17,26 +17,36 @@ import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
 function Nav() {
-  // const user = useSelector((store) => store.user);
+
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const settings = ['User Dashboard', <p id="signOut" onClick={() => dispatch({ type: 'LOGOUT' })}>Sign Out</p>];
+
+  let pages = [];
+  if (user.id === undefined || user.id === null) {
+    pages = [<Link to="/about">About</Link>, <Link to="/login">Login / Register</Link>];
+  }
+  else if (user.id) {
+    pages = [<Link to="/about">About</Link>, <Link to='/home'>Home</Link>, <Link to="/info">Info Page</Link>, <Link to="/add">Add Vehicle</Link> ];
+  }
+  
 
   // return (
   //   <div className="nav">
-  //     <Link to="/home">
-  //       <h2 className="nav-title">Prime Solo Project</h2>
-  //     </Link>
+      // <Link to="/home">
+      //   <h2 className="nav-title">Prime Solo Project</h2>
+      // </Link>
   //     <div>
   //       {/* If no user is logged in, show these links */}
 
   //       {(user.id === undefined || user.id === null) &&
 
   //         // If there's no user, show login/registration links
-  //         <Link className="navLink" to="/login">
-  //           Login / Register
-  //         </Link>
+          // <Link className="navLink" to="/login">
+          //   Login / Register
+          // </Link>
   //       }
 
   //       {/* If a user is logged in, show these links */}
@@ -46,13 +56,13 @@ function Nav() {
   //             Home
   //           </Link>
 
-  //           <Link className="navLink" to="/info">
-  //             Info Page
-  //           </Link>
+            // <Link className="navLink" to="/info">
+            //   Info Page
+            // </Link>
 
-  //           <Link className="navLink" to="/add">
-  //             Add Vehicle
-  //           </Link>
+            // <Link className="navLink" to="/add">
+            //   Add Vehicle
+            // </Link>
 
   //           <LogOutButton className="navLink" />
 
@@ -91,21 +101,20 @@ function Nav() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          {/* <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            LOGO
-          </Typography> */}
-
+          
+          {/* LOGO */}
+          
+          <Link to="/home">
           <img src="/images/LYSTR-logo.png" id="logo"/>
-
+          </Link>
+          
 
           
 
+          {/* HAMBURGER MENU BUTTON WHEN SMALL SCREEN */}
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+
+            {/* HAMBURGER BUTTON AND ICON */}
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -116,6 +125,8 @@ function Nav() {
             >
               <MenuIcon />
             </IconButton>
+
+            {/* HAMBURGER MENU DROPDOWN OPTIONS */}
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
@@ -141,14 +152,18 @@ function Nav() {
               ))}
             </Menu>
           </Box>
+          
+          {/* EXTRA STUFF THAT SAID LOGO ON SMALL SCREEN - NOT NEEDED? */}
           <Typography
             variant="h6"
             noWrap
             component="div"
             sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
           >
-            LOGO
+            {/* LOGO */}
           </Typography>
+
+          {/* LINK OPTIONS LARGE SCREEN */}
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
@@ -161,12 +176,17 @@ function Nav() {
             ))}
           </Box>
 
+          {/* PROFILE PHOTO AND LOGOUT DROPDOWN ON CLICK */}
           <Box sx={{ flexGrow: 0 }}>
+
+            {/* PHOTO AND OPEN DROPDOWN ON CLICK */}
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
+
+            {/* MENU ITEMS */}
             <Menu
               sx={{ mt: '45px' }}
               id="menu-appbar"
@@ -189,6 +209,7 @@ function Nav() {
                 </MenuItem>
               ))}
             </Menu>
+
           </Box>
         </Toolbar>
       </Container>
