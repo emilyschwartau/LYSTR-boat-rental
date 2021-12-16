4ba8f6a6aa0efb43b39da692b34710669ea3c001
SJCMN <sam.clavette@gmail.com>
2021-12-16 11:12:30 -0600

    fixing merge

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require("./strategies/user.strategy");
 const userRouter = require("./routes/user.router");
 const vehicleRouter = require("./routes/vehicle.router");
 const dataRouter = require("./routes/data.router");
+const searchRouter = router("./routes/search.router")
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -27,6 +28,7 @@ app.use(passport.session());
 app.use("/api/user", userRouter);
 app.use("/api/vehicle", vehicleRouter);
 app.use("/api/data", dataRouter);
+app.use("/api/search", searchRouter);
 
 // Serve static files
 app.use(express.static("build"));
