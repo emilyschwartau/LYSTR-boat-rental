328e1874a23749e93c079dfb7fca31282f5afca8
SJCMN <sam.clavette@gmail.com>
2021-12-20 16:48:09 -0600

    componentizing locatin button

--- src/components/LocationComboBox/LocationComboBox.jsx
+++ src/components/LocationComboBox/LocationComboBox.jsx
@@ -9,49 +9,10 @@ function LocationComboBox() {
     const dispatch = useDispatch()
     const { cities } = useSelector((store) => store.data);
 
-
-    // removes all keys from cities table 
-    // to use as labels for auto complete
-
-    // const cityLabels = []
-
-    // function removeKeys(obj) {
-        
-    //     let keyArr = [];
-    //     let concatArr = [];
-    //     for (let k of obj) {
-    //         if (typeof (k) === 'number') {
-    //             // convert zip codes to string
-    //             keyArr.push(Object.values(k).toString())
-    //         } else {
-    //             // add each item to a new array without keys
-    //             keyArr.push(Object.values(k))
-    //         }
-    //     }
-    //     // concat all new arrays with no keys to one array
-    //     for (let i of keyArr) {
-    //         concatArr = concatArr.concat(i)
-    //     }
-
-    //     // check all instances and remove duplicates
-    //     const removeDuplicates = concatArr.reduce(function (a, b) {
-    //         if (a.indexOf(b) === -1) {
-    //             a.push(b)
-    //         }
-    //         return a
-    //     }, [])
-        
-
-    //     for (let i of removeDuplicates){
-    //         cityLabels.push({"label": i})
-    //     }
-
-    //     return cityLabels
-    // }
-
-
-    // removeKeys(cities)
-    // console.log('in remove keys combo box:',cityLabels)
+    const handleLocationChange = (e) => {
+        setSearch({ ...search, location: e.target.value })
+        dispatch({ type: 'SET_SEARCH_LOCATION', payload: e.target.value });
+      }
 
     React.useEffect(() => {
         dispatch({ type: 'FETCH_CITY_LIST' });
@@ -62,20 +23,15 @@ function LocationComboBox() {
             <Autocomplete
                 disablePortal
                 autoComplete={true}
-                // autoSelect={true}
+                required
+                // value={search.location}
                 id="combo-box-demo"
                 options={cities}
-                sx={{ width: 300 }}
                 renderInput={(params) =>
-                    <TextField {...params} label="Cities" />
+                    <TextField {...params} label="Location" />
                 }
-            getOptionLabel={(option) => option.label}
-            // returns text for search box
-            // getOptionLabel={(option) => `${option.city} ${option.county}`}
-            // renders options passed into getOptionLabel
-            // renderOption={(option) => 
-            //     {return (<h4>{option.label}</h4>)}
-            // }
+                getOptionLabel={(option) => option.label}
+
             />
             <br />
         </div>
