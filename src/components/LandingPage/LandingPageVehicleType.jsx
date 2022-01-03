d55bc45a2f8ee965f39f99ad439fed31db714494
AustinKammerer <austin4191@gmail.com>
2022-01-03 10:27:11 -0600

    tweaked reservation popup

--- src/components/LandingPage/LandingPageVehicleType.jsx
+++ src/components/LandingPage/LandingPageVehicleType.jsx
@@ -4,12 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import SearchIcon from '@mui/icons-material/Search';
 import { useHistory } from 'react-router-dom';
 
-
 function LandingPageVehicleType() {
   const dispatch = useDispatch();
   const history = useHistory();
   const vehicleList = useSelector((store) => store.data.types);
-  const vehicleType = useSelector((store) => store.search.searchQuery.vehicleType);
+  const vehicleType = useSelector(
+    (store) => store.search.searchQuery.vehicleType
+  );
   const startDate = useSelector((store) => store.search.searchQuery.startDate);
   const location = useSelector((store) => store.search.searchQuery.location);
 
@@ -32,7 +33,7 @@ function LandingPageVehicleType() {
   };
 
   const checkId = () => {
-    if (vehicleType === undefined) {
+    if (vehicleType === '') {
       buttonText = 'Boats';
     } else if (vehicleType === 'Fishing') {
       buttonText = 'Fishing Boats';
@@ -77,13 +78,13 @@ function LandingPageVehicleType() {
             {vehicleList?.map((vehicle) => (
               <Card
                 key={vehicle.id}
-                elevation={ vehicleType === vehicle.name ? 6 : 2 }
+                elevation={vehicleType === vehicle.name ? 6 : 2}
                 onClick={() => {
                   console.log(vehicle);
                   dispatch({
                     type: 'SET_SEARCH_VEHICLE_TYPE',
                     payload: vehicle.name,
-                  })
+                  });
                 }}
                 sx={{ margin: '1em', height: '20vh', width: '20vw' }}
               >
