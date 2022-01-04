97fd5ec61e606d8760846333285fc01aa74b0236
Emily Schwartau <emilyschwartau+1@gmail.com>
2022-01-04 11:05:41 -0600

    back button url fixes

--- src/views/VehicleDetailsPage/VehicleDetailsPage.jsx
+++ src/views/VehicleDetailsPage/VehicleDetailsPage.jsx
@@ -66,7 +66,7 @@ export default function VehicleDetailsPage() {
   return (
     <>
 
-  <Button id="backToGalleryBtn" variant="contained" onClick={() => history.push(`/gallery`)}>
+  <Button id="backToGalleryBtn" variant="contained" onClick={() => history.goBack()}>
         Back to Search Results
   </Button>
   <Box sx={{ margin: 'auto', padding: '1em', width: '80%' }}>
