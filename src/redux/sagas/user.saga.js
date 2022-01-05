5ef8415a899480920ea96c7e496988fa2ffaf95b
AustinKammerer <austin4191@gmail.com>
2022-01-05 17:51:20 -0600

    user profile info and picture may be updated

--- src/redux/sagas/user.saga.js
+++ src/redux/sagas/user.saga.js
@@ -24,8 +24,36 @@ function* fetchUser() {
   }
 }
 
+function* updateProfile(action) {
+  try {
+    yield put({ type: 'START_LOADING' });
+    yield axios.put('/api/user', action.payload);
+    yield put({ type: 'FETCH_USER' });
+    yield put({ type: 'STOP_LOADING' });
+    console.log('Profile Updated!');
+  } catch (error) {
+    console.log('error updating profile:', error);
+    yield put({ type: 'PUT_ERROR' });
+  }
+}
+
+function* updateProfilePic(action) {
+  const { newPic, oldPic } = action.payload;
+  const formData = new FormData();
+  formData.append('profilePic', newPic);
+  formData.append('oldPic', oldPic);
+  try {
+    yield put({ type: 'START_LOADING' });
+    yield axios.put('/api/user/pic', formData);
+    yield put({ type: 'STOP_LOADING' });
+    yield put({ type: 'FETCH_USER' });
+  } catch (error) {}
+}
+
 function* userSaga() {
   yield takeLatest('FETCH_USER', fetchUser);
+  yield takeLatest('UPDATE_PROFILE', updateProfile);
+  yield takeLatest('UPDATE_PROFILE_PIC', updateProfilePic);
 }
 
 export default userSaga;
