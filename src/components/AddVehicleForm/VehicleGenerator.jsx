d675585b603cc6d4c48dfc0742441ff95d7c11a6
SJCMN <sam.clavette@gmail.com>
2022-01-09 19:03:15 -0600

    add spread operator for address

--- src/components/AddVehicleForm/VehicleGenerator.jsx
+++ src/components/AddVehicleForm/VehicleGenerator.jsx
@@ -15,7 +15,7 @@ function VehicleGenerator({ handleChange, validateNumber }) {
     const newVehicle = () => {
       const newAddress = address[Math.floor(Math.random() * address.length)] 
 
-      return {...vehicleModel, ...vehicleExamples[Math.floor(Math.random() * vehicleExamples.length)], street: newAddress.street, city: newAddress.city, zip:newAddress.zip}
+      return {...vehicleModel, ...vehicleExamples[Math.floor(Math.random() * vehicleExamples.length)], ...newAddress}
     }
 
     dispatch({ type: 'SET_VEHICLE_FORM_INPUTS', payload : newVehicle() })
