f06dcb7336cdb59a404c70267532908cdbfa7a31
SJCMN <sam.clavette@gmail.com>
2022-01-09 19:53:07 -0600

    calendar tracking available days and opening on first available day

--- src/components/UserPage/ListingCalendar.jsx
+++ src/components/UserPage/ListingCalendar.jsx
@@ -1,41 +1,16 @@
 import React from 'react';
 import { Calendar, DateObject } from 'react-multi-date-picker';
-import { useDispatch, useSelector } from 'react-redux';
-import useQuery from '../../hooks/useQuery';
 import FormControl from '@mui/material/FormControl';
-
 
 
 function ListingCalendar ({vehicle}) {
 
-    const dispatch = useDispatch();
-    const { bookingInput } = useSelector((store) => store.rental);
-    const { vehicleInfo } = useSelector((store) => store.vehicle);
+    // calendar to show when vehicle is available on my listings tab
+    // vehicle is the mapped available vehicle from listedVehiclesByOwner array
 
-    const { allReservationsById } = useSelector((store) => store.vehicle);
-
-    const availability = vehicleInfo.availability
-    const date = vehicle.rentalData[0].rentalDate
-
-    console.log(date, availability, bookingInput, allReservationsById)
-    // const date = vehicle.allReservationsById[0].dateRented
- 
-
-    // console.log( date, date.split('-'))
-    // const query = useQuery();
-    // const date = query.get('date');
-
-    // date = '02-03-2022'
+    const availability = vehicle.availability
+    const date =  vehicle.availability[0]
   
-    // const user = useSelector((store) => store.user);
-    // const [dateInput, setDateInput] = React.useState('');
-  
-    // for requiring a date selection
-    // const [noDate, setNoDate] = React.useState(false);
-  
-    React.useEffect(() => dispatch({ type: 'CLEAR_BOOKING_INPUT' }), []);
-
-
     return (
         <FormControl margin="normal">
         <Calendar
@@ -43,10 +18,6 @@ function ListingCalendar ({vehicle}) {
             .set('year', date.split('-')[2])
             .set('month', date.split('-')[0])
             .set('day', date.split('-')[1])}
-        //   value={new DateObject()
-        //     .set('year', bookingInput.date.split('-')[2])
-        //     .set('month', bookingInput.date.split('-')[0])
-        //     .set('day', bookingInput.date.split('-')[1])}
           numberOfMonths={1}
           mapDays={({ date, today }) => {
             const isAvailable = availability?.includes(
