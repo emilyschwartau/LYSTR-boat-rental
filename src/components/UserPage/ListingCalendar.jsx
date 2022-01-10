09b7a2f058af1e403ceb0232b5d591c39e0ad005
SJCMN <sam.clavette@gmail.com>
2022-01-09 20:27:11 -0600

    update calendar on my listings view

--- src/components/UserPage/ListingCalendar.jsx
+++ src/components/UserPage/ListingCalendar.jsx
@@ -7,17 +7,34 @@ function ListingCalendar ({vehicle}) {
 
     // calendar to show when vehicle is available on my listings tab
     // vehicle is the mapped available vehicle from listedVehiclesByOwner array
+    const availability = vehicle.availability;
+    const date =  vehicle.availability[0];
+    const today = new DateObject();
 
-    const availability = vehicle.availability
-    const date =  vehicle.availability[0]
+    // this is the current month
+    const { month } = today;
+
+    // use date object and set the value to the first available rental day
+    const firstAvailable = 
+    
+    today
+    .set('year', date.split('-')[2])
+    .set('month', date.split('-')[0])
+    .set('day', date.split('-')[1]);
+
+    // currentDate compares the number of current month to first month listed as available
+    // the current date on the calendar will be the greater of these
+    // current date sets the month the calendar opens when initially displayed
   
     return (
         <FormControl margin="normal">
         <Calendar
-          currentDate={new DateObject()
-            .set('year', date.split('-')[2])
-            .set('month', date.split('-')[0])
-            .set('day', date.split('-')[1])}
+          currentDate={
+            firstAvailable.month.number > month.number? 
+            firstAvailable
+            :
+            firstAvailable.month
+        }
           numberOfMonths={1}
           mapDays={({ date, today }) => {
             const isAvailable = availability?.includes(
