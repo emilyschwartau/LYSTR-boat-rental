42fe9091d5c9381fd43d23e9998fad9a07964101
AustinKammerer <austin4191@gmail.com>
2022-01-10 10:13:34 -0600

    removed unused var

--- server/routes/vehicle.router.js
+++ server/routes/vehicle.router.js
@@ -16,8 +16,6 @@ const {
   rejectUnauthenticated,
 } = require('../modules/authentication-middleware');
 
-
-
 /*
  * GET routes
  */
@@ -73,7 +71,7 @@ router.get('/allVehiclesListed/:userId', rejectUnauthenticated, (req, res) => {
   const query = `
   SELECT "vehicle"."id" AS "vehicleId", "user"."username" AS "ownedBy", "type"."name" AS "type", "title", "make", "model", "year", "length", "capacity", "horsepower", "street", "city", "state", "zip", "description", "cabins", "heads", "daily_rate" AS "dailyRate",
     (select JSON_AGG("image_path") as "photos" from "photos" where "vehicle"."id" = "photos"."vehicle_id"),
-    (select JSON_AGG("date_available") as "availability" from "availability" where "vehicle"."id" = "availability"."vehicle_id"),
+    (select JSON_AGG("date_available") as "availability" from "availability" where "vehicle"."id" = "availability"."vehicle_id" and "is_rented" = FALSE),
     (select JSON_AGG("name") as "features" from "features" join "vehicle_features" on "features"."id" = "vehicle_features"."feature_id" where "vehicle"."id" = "vehicle_features"."vehicle_id") FROM "vehicle" 
   JOIN "type" ON "vehicle"."type_id" = "type"."id" 
   JOIN "user" ON "vehicle"."owned_by" = "user"."id"
@@ -133,7 +131,6 @@ router.get('/uploads/:key', (req, res) => {
   // pipe the read stream to the client
   readStream.pipe(res);
 });
-
 
 /*
  * POST routes
@@ -203,7 +200,7 @@ router.post('/features/:vehicleId', rejectUnauthenticated, (req, res) => {
   const { features } = req.body;
   const { vehicleId } = req.params;
 
-  console.log('vehicle router:',vehicleId, features)
+  console.log('vehicle router:', vehicleId, features);
 
   let query = `
     INSERT INTO "vehicle_features" ("vehicle_id", "feature_id")
--- src/components/UserPage/ListingCalendar.jsx
+++ src/components/UserPage/ListingCalendar.jsx
@@ -2,53 +2,42 @@ import React from 'react';
 import { Calendar, DateObject } from 'react-multi-date-picker';
 import FormControl from '@mui/material/FormControl';
 
+function ListingCalendar({ vehicle }) {
+  // calendar to show when vehicle is available on my listings tab
+  // vehicle is the mapped available vehicle from listedVehiclesByOwner array
+  const availability = vehicle.availability.sort();
+  const today = new DateObject();
 
-function ListingCalendar ({vehicle}) {
+  // find the next available rental date after today
+  const findEarliest = () => {
+    for (let date of availability) {
+      let dateObj = new DateObject(date)
+        .set('year', date.split('-')[2])
+        .set('month', date.split('-')[0])
+        .set('day', date.split('-')[1]);
+      if (dateObj.dayOfYear >= today.dayOfYear) {
+        return dateObj;
+      }
+    }
+    return today;
+  };
 
-    // calendar to show when vehicle is available on my listings tab
-    // vehicle is the mapped available vehicle from listedVehiclesByOwner array
-    const availability = vehicle.availability;
-    const date =  vehicle.availability[0];
-    const today = new DateObject();
-
-    // this is the current month
-    const { month } = today;
-
-    // use date object and set the value to the first available rental day
-    const firstAvailable = 
-    
-    today
-    .set('year', date.split('-')[2])
-    .set('month', date.split('-')[0])
-    .set('day', date.split('-')[1]);
-
-    // currentDate compares the number of current month to first month listed as available
-    // the current date on the calendar will be the greater of these
-    // current date sets the month the calendar opens when initially displayed
-  
-    return (
-        <FormControl margin="normal">
-        <Calendar
-          currentDate={
-            firstAvailable.month.number > month.number? 
-            firstAvailable
-            :
-            firstAvailable.month
-        }
-          numberOfMonths={2}
-          mapDays={({ date, today }) => {
-            const isAvailable = availability?.includes(
-              date.format('MM-DD-YYYY')
-            );
-            if (!isAvailable || date.dayOfYear < today.dayOfYear)
-              return {
-                disabled: true,
-                style: { color: '#ccc' },
-              };
-          }}
-        />
-      </FormControl>
-    )
+  return (
+    <FormControl margin="normal">
+      <Calendar
+        currentDate={findEarliest()}
+        numberOfMonths={2}
+        mapDays={({ date, today }) => {
+          const isAvailable = availability?.includes(date.format('MM-DD-YYYY'));
+          if (!isAvailable || date.dayOfYear < today.dayOfYear)
+            return {
+              disabled: true,
+              style: { color: '#ccc' },
+            };
+        }}
+      />
+    </FormControl>
+  );
 }
 
-export default ListingCalendar
+export default ListingCalendar;
